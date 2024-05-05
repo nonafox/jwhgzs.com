5735638a118e73c30dab1c3cca99ba9fe9680ca5
jwhgzs <admin@jwhgzs.com>
2024-05-05 13:53:59 +0800

    upload

--- middleware/domains.global.ts
+++ middleware/domains.global.ts
@@ -1,7 +1,8 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     let host = useRequestURL().host.split(':')[0], parts = host.split('.'),
         host_ = host.replaceAll('.', '_')
-    let dest_ = to.fullPath.toLowerCase()
+    let dest = to.fullPath,
+        dest_ = dest.toLowerCase()
     if (parts.length < 3)
         return navigateTo(useRequestURL().protocol + '//www.' + host + dest, { external: true })
     else if (dest_.startsWith('/_nuxt') || dest_.startsWith('/robots.txt'))
